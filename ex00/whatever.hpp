8c56e7438c1ed6bc0329d928abbb22b9e1671798
Stefan Ruff <sruff@3-H-1.42heilbronn.de>
2026-05-14 16:07:21 +0200

    inital commit

---
+++ ex00/whatever.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/14 15:17:10 by sruff             #+#    #+#             */
+/*   Updated: 2026/05/14 16:06:49 by sruff            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T> void swap(T &a, T &b)
+{
+
+}
+
+template <typename T> T min(T const &a, T const &b)
+{
+
+	return (b);
+}
+
+template <typename T> T max(T const &a, T const &b)
+{
+
+	return (b);
+}
+
+#endif
