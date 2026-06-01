1b2e7bdbc94726b2e7507d1b9b2ba75bcf0df4f5
Stefan Ruff <sruff@3-G-4.42heilbronn.de>
2026-06-01 14:44:37 +0200

    added test

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -30,12 +30,14 @@ int main( void )
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
+	std::cout << "------------------" << std::endl;
 	// equal values: min/max must return the second argument
 	int x = 42;
 	int y = 42;
 	std::cout << "min( equal ) = " << ::min( x, y ) << std::endl;
 	std::cout << "max( equal ) = " << ::max( x, y ) << std::endl;
 
+	std::cout << "------------------" << std::endl;	
 	//int a = 2;
 	//int b = 3;
 	char ca = 'a';
@@ -46,5 +48,13 @@ int main( void )
 	std::cout << "min( ca, cb ) = " << ::min( ca, cb ) << std::endl;
 	std::cout << "max( ca, cb ) = " << ::max( ca, cb ) << std::endl;
 
+	std::cout << "------------------" << std::endl;
+	char *pa = &ca;
+	char *pb = &cb;
+	std::cout << "adress &ca = " << &pa << std::endl << "adress &cb = " << &pb << std::endl;
+	std::cout << "ca = " << pa << ", cb = " << pb << std::endl;
+	::swap( pa, pb);
+	std::cout << "adress &ca = " << &pa << std::endl << "adress &cb = " << &pb << std::endl;
+	std::cout << "ca = " << pa << ", cb = " << pb << std::endl;
 	return 0;
 }
