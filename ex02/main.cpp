27bc82e75b12df022bc0319f3c50eb1e603053a4
Stefan Ruff <sruff@3-G-7.42heilbronn.de>
2026-06-02 17:38:48 +0200

    since subject demands onl a single .cpp file i had to integrate both main.cpp into one

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,18 @@
 /*   By: sruff <sruff@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/01 14:46:53 by sruff             #+#    #+#             */
-/*   Updated: 2026/06/02 17:11:32 by sruff            ###   ########.fr       */
+/*   Updated: 2026/06/02 17:32:49 by sruff            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+#define MAX_VAL 750
 
 template <typename T> void dump(Array<T> const &arr)
 {
@@ -23,6 +28,51 @@ template <typename T> void dump(Array<T> const &arr)
 
 int	main(void)
 {
+	int	*mirror;
+		const int value = rand();
+
+	Array<int> numbers(MAX_VAL);
+	mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	// SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return (1);
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete[] mirror;	
 	std::cout << "---- empty array ----" << std::endl;
 	Array<int> empty;
 	std::cout << "size: " << empty.size() << std::endl;
